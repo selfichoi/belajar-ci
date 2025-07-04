007cdabce1c466b32ae3cde411930b2475b34ee6
selfichoi <selfi456meteseh@gmail.com>
2025-07-04 19:50:45 +0700

    first commit

---
+++ app/Views/layout_clear.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Login - Toko</title>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600|Nunito:600|Poppins:500" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?= base_url('NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('NiceAdmin/assets/css/style.css') ?>" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+        <?= $this->renderSection('content') ?>
+    </main>
+
+    <script src="<?= base_url('NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('NiceAdmin/assets/js/main.js') ?>"></script>
+</body>
+</html>
